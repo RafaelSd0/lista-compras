578dca4f00dc0745e818c7830c076f2d95ab1987
RafaelSd0 <player08901@gmail.com>
2024-10-01 19:45:53 -0300

    Configurações iniciais

---
+++ src/app/item-lista/item-lista.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-item-lista',
+  standalone: true,
+  imports: [],
+  templateUrl: './item-lista.component.html',
+  styleUrl: './item-lista.component.css'
+})
+export class ItemListaComponent {
+
+}
