e581eff394cdbd1e2e8bb4891441b78e807fc27d
RafaelSd0 <player08901@gmail.com>
2024-10-01 21:45:54 -0300

    Formulario Inicial criado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
+import { ItemListaComponent } from './item-lista/item-lista.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, ItemListaComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/item-lista/item-lista.component.ts
+++ src/app/item-lista/item-lista.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-item-lista',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './item-lista.component.html',
   styleUrl: './item-lista.component.css'
 })
 export class ItemListaComponent {
 
+  item: string = '';
+
+  onSubmit(form: any) {
+    if (form.valid) {
+      console.log('Item adicionado:', this.item);
+      // Lógica para adicionar o item à lista
+    }
+  }
 }
