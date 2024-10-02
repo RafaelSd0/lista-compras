10e0c4b43faf85522c15d3657b0bf0f9a5e466e3
RafaelSd0 <player08901@gmail.com>
2024-10-01 21:56:37 -0300

    alteração no itemLista

--- src/app/item-lista/item-lista.component.ts
+++ src/app/item-lista/item-lista.component.ts
@@ -1,21 +1,25 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
+import { CommonModule } from '@angular/common';
+
 @Component({
   selector: 'app-item-lista',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule,CommonModule],
   templateUrl: './item-lista.component.html',
   styleUrl: './item-lista.component.css'
 })
 export class ItemListaComponent {
 
   item: string = '';
+  itemLista: string[] = [];
 
   onSubmit(form: any) {
     if (form.valid) {
-      console.log('Item adicionado:', this.item);
-      // Lógica para adicionar o item à lista
+      this.itemLista.push(this.item);
+      this.item = '';
+      console.log('Item adicionado:', this.itemLista);
     }
   }
 }
