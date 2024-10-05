f336088df62d758b25fbbcd435a0ce9155f5c551
RafaelSd0 <player08901@gmail.com>
2024-10-05 12:43:59 -0300

    Estilo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { ItemListaComponent } from './item-lista/item-lista.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, ItemListaComponent],
+  imports: [RouterOutlet, HeaderComponent, ItemListaComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
