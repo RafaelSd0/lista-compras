cf2aa64af30156e583e7022a5a92f7d6cb6cecf1
RafaelSd0 <player08901@gmail.com>
2024-11-05 13:35:04 -0300

    adiciona httpClient

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,7 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideAuth0 } from '@auth0/auth0-angular';
 import { provideRouter } from '@angular/router';
-
+import { provideHttpClient } from '@angular/common/http';
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
@@ -13,7 +13,10 @@ export const appConfig: ApplicationConfig = {
 
   provideAnimationsAsync(),
 
+  provideHttpClient(),
+
   provideAuth0({
+
     domain: 'http://dev-h1akqwp5consne5a.us.auth0.com',
 
     clientId: 'f22egNDfmBZb5tHq4llqS9vrySRZpynp',
@@ -22,8 +25,15 @@ export const appConfig: ApplicationConfig = {
 
     redirect_uri: window.location.origin,
 
+    audience: 'https://dev-h1akqwp5consne5a.us.auth0.com/api/v2/',
+
+    scope: 'openid profile email offline_access',
+
     },
 
+    useRefreshTokens: true,
+
+    cacheLocation: 'localstorage',
 })
 
 ]
