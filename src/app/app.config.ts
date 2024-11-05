ee435a0ebddbeca71c9fc78b6c2615be9ac8da6e
RafaelSd0 <player08901@gmail.com>
2024-11-04 22:06:20 -0300

    Registrar o Auth0 no Angular

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,31 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideAuth0 } from '@auth0/auth0-angular';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync()]
+  providers: [
+  provideZoneChangeDetection({ eventCoalescing: true }),
+
+  provideRouter(routes),
+
+  provideAnimationsAsync(),
+
+  provideAuth0({
+    domain: 'http://dev-h1akqwp5consne5a.us.auth0.com',
+
+    clientId: 'f22egNDfmBZb5tHq4llqS9vrySRZpynp',
+
+    authorizationParams: {
+
+    redirect_uri: window.location.origin,
+
+    },
+
+})
+
+]
+
 };
