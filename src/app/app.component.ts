40c0fe133333283d23df3b6c8ef7106cd928542d
RafaelSd0 <player08901@gmail.com>
2024-11-04 21:53:43 -0300

    login component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
-import { ItemListaComponent } from './item-lista/item-lista.component';
 import { FooterComponent } from './footer/footer.component';
+import { LoginComponent } from "./login/login.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, ItemListaComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+login() {
+throw new Error('Method not implemented.');
+}
+logout() {
+throw new Error('Method not implemented.');
+}
+
+}
