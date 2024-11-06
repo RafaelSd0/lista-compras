958aa732a42fb83ad085bb30b66537e78636276a
RafaelSd0 <player08901@gmail.com>
2024-11-06 14:39:37 -0300

    rota para home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { AuthGuard } from '@auth0/auth0-angular';
 import { ItemListaComponent } from './item-lista/item-lista.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
   {path:'lista', component:ItemListaComponent, canActivate:[AuthGuard]},
-  {path:'**', redirectTo:'lista'}
+  {path:'home', component:HomeComponent},
+  {path:'**', redirectTo:'home'}
 ];
