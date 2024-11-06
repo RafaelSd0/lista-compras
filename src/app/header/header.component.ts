22d3894b5fb1ff19e6624a00e5f10af55c059d25
RafaelSd0 <player08901@gmail.com>
2024-11-06 14:54:23 -0300

    alteração no header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,19 +1,33 @@
 import { CommonModule } from '@angular/common';
 import { Component, Inject } from '@angular/core';
 import {MatToolbarModule} from '@angular/material/toolbar';
-import { AuthService } from '@auth0/auth0-angular';
+import { AuthService, User } from '@auth0/auth0-angular';
 import { DOCUMENT } from '@angular/common';
+import { RouterLink } from '@angular/router';
+
+
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatToolbarModule, CommonModule],
+  imports: [MatToolbarModule, CommonModule, RouterLink],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
 
+  profile!: User | null | undefined;
+
   constructor(public auth: AuthService, @Inject(DOCUMENT) private document: Document) {}
+
+  ngOnInit(): void {
+
+    this.auth.user$.subscribe((profile) => {
+
+    this.profile = profile;
+
+    });
+  }
 
   login() {
     this.auth.loginWithRedirect();
@@ -25,5 +39,18 @@ export class HeaderComponent {
         returnTo: this.document.location.origin
       }
     });
+  }
+
+  //Mostrar menu mara usuarios do Mobile
+  mostrarMenu(aberto:boolean): void{
+    aberto = false;
+    const menu = this.document.querySelector('#menu') as HTMLDivElement;
+    if (aberto) {
+      menu.classList.remove('hidden');
+      menu.classList.add('flex');
+    } else {
+      menu.classList.remove('flex');
+      menu.classList.add('hidden');
+    }
   }
 }
